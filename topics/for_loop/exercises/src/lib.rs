30f3e9688627279e49edcdd31a0d1de19836d0e0
Dani Gámez Franco <gmzcodes@outlook.com>
2025-09-25 23:51:38 +0200

    Finish Control Flow section.

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,19 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum: i32 = 0;
+
+    for x in nums.iter() {
+        sum += x;
+    }
+
+    return sum;
 }
 
-pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+pub fn fill(value: u32, n: usize) -> Vec<u32> {
+    let mut v: Vec<u32> = Vec::new();
+
+    for _ in 0..n {
+        v.push(value);
+    }
+
+    return v;
 }
