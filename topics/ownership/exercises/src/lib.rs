b4e029da6c83be89caa272bbbf5848ab1334cd20
Dani Gámez Franco <gmzcodes@outlook.com>
2025-09-26 20:11:45 +0200

    Finish Borrowing section.

--- topics/ownership/exercises/src/lib.rs
+++ topics/ownership/exercises/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn exercise_1() {
     let s = "rust".to_string();
     let s1 = s;
-    let s2 = s;
+    // let s2 = s;
     println!("{s1}");
 }
 
@@ -9,18 +9,30 @@ pub fn exercise_2() {
     let s = "rust".to_string();
     {
         let s1 = s;
+        
+        // Can also be fixed doing this:
+        // let s1 = s.clone();
+
         println!("{s1}");
     }
-    println!("{s}");
+
+    // println!("{s}");
 }
 
+// Can also be fixed doing this:
+// fn take(s: &String) {
 fn take(s: String) {
     println!("take {s}");
 }
 
 pub fn exercise_3() {
     let s = "rust".to_string();
-    take(s);
+
+    // Can also be fixed doing this:
+    // take(&s);
+
+    // take(s);
+
     println!("{s}");
     println!("{s}");
 }
